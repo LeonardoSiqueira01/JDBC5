11465157cc763f9f1a22399a747fe7a2116a5236
Leonardo Siqueira <leonardo.gabriel01@hotmail.com>
2023-03-26 14:53:07 -0300

    Deletando dados e tratando exceção de integridade referencial

---
+++ src/application/Program.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB;
+import db.DbIntegrityException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Connection conn = null;
+		PreparedStatement st = null;
+		try {
+			conn = DB.getConnection();
+			st = conn.prepareStatement("Delete from department "
+					+ "where "
+					+ "Id = ?");
+			st.setInt(1, 5);
+			
+			int rowsAffected = st.executeUpdate();
+			System.out.println("Done! Rows affected: "+rowsAffected);
+		}
+		catch(SQLException e) {
+			throw new DbIntegrityException(e.getMessage());
+			}
+			finally {
+			DB.closeStatment(st);
+			DB.closeConnection();
+		}
+		}
+	}
